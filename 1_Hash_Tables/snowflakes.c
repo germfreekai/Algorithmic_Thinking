cc4903b6d3fee436233e66500933083557f776a1
Rene Garcia Aviles <renegarc@TR-9ZX5VL3>
2024-07-08 17:30:17 -0600

    Add first iteration for snowflakes
    Added first iteration for snowflakes problem, this iteration created the
    function 'are_identical' to compare 2 snowflakes.
    
    Signed-off-by: Rene Garcia Aviles <renegarc@TR-9ZX5VL3>

---
+++ 1_Hash_Tables/snowflakes.c
@@ -0,0 +1,97 @@
+/*
+ * Verify whether two snowflakes are identical.
+ * A snowflake is made out of 6 integers.
+ * A snowflake could be identical if by spinning the given
+ * ints, either to the left or to the right we get the same
+ * order (like a circle)
+ * You will receive N snowflakes, time limit 2 seconds
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Sample easy function
+void identify_identical(int *values, int size);
+
+// Main function
+int are_identical(int *snow1, int *snow2);
+// Helper functions
+int identical_right(int *snow1, int *snow2, int start_s2);
+int identical_left(int *snow1, int *snow2, int start_s2);
+
+int main(void)
+{
+    // int arr[5] = {1, 2, 3, 1, 5};
+    // identify_identical(arr, 5);
+
+    // sample
+    int snow1[6] = {1, 2, 3, 4, 5, 6};
+    int snow2[6] = {4, 5, 6, 1, 2, 3};
+    int snow3[6] = {4, 5, 7, 1, 2, 3};
+
+    if (are_identical(snow1, snow3))
+        fprintf(stdout, "Snowflakes are identical!\n");
+    else
+        fprintf(stderr, "Snowflakes are NOT identical!\n");
+
+    return EXIT_SUCCESS;
+}
+
+void identify_identical(int *values, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (values[i] == values[j])
+            {
+                fprintf(stdout, "Twin integers found!\n");
+                return;
+            }
+        }
+    }
+    fprintf(stderr, "No two integers are alike\n");
+}
+
+int identical_right(int *snow1, int *snow2, int start_s2)
+{
+    int offset;
+    for (offset = 0; offset < 6; offset++)
+    {
+        /*
+         * Mod returns the remaining, so if we divide we will
+         * always get a smaller number than snow2 len
+         */
+        if (snow1[offset] != snow2[(start_s2 + offset) % 6])
+            return 0;
+    }
+    return 1;
+}
+
+int identical_left(int *snow1, int *snow2, int start_s2)
+{
+    int offset;
+    int snow2_index;
+
+    for (offset = 0; offset < 6; offset++)
+    {
+        snow2_index = start_s2 - offset;
+        if (snow2_index < 0)
+            snow2_index = snow2_index + 6;
+        if (snow1[offset] != snow2[snow2_index])
+            return 0;
+    }
+    return 1;
+}
+
+int are_identical(int *snow1, int *snow2)
+{
+    for (int start = 0; start < 6; start++)
+    {
+        if (identical_right(snow1, snow2, start))
+            return 1;
+        if (identical_left(snow1, snow2, start))
+            return 1;
+    }
+    return 0;
+}
